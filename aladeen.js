aa5de9fe202485c03a759923361b142f093f82cb
unknown <john@jjmasse.com>
2013-07-14 10:34:18 -0400

    Added in the ability to set some options and chaining

--- aladeen.js
+++ aladeen.js
@@ -1,5 +1,24 @@
-$.fn.aladeen = function() {
-		console.log('Wassup');
+$.fn.aladeen = function(options) {
+	console.log('Wassup');
     console.log('▬▬ι═══════ﺤ -═══════ι▬▬');
-    console.log('٩(͡๏̯͡๏)۶');
+    console.log('٩(͡๏̯͡๏)۶'); // <-- WTH IS THIS? HAHAHA
+
+	/* ---
+	ALADEEN JS COMES WITH A COUPLE OF SPECIAL OPTIONS FOR YOU
+	MOST DEVELOPERS PREFER TO SET THEIR ALADEEN OPTION 100% OF THE TIME 
+	--- */
+	var defaults = {
+			aladeen: 'slideLeft',
+			aladeen: 'fadeIn',
+			aladeen : false,
+			aladeen: true,
+			aladeen: 'Aladeen'
+		},
+		options = $.extend(defaults, options);
+
+	if(options.aladeen !== 'Aladeen'){
+		options.aladeen = 'It\'s Aladeen';
+	}
+	console.log(options); // PRINTS OUT THE OPTIONS
+	return this; // WILL ALLOW FURTHER CHAINING
 };
--- demo.js
+++ demo.js
@@ -1,4 +1,7 @@
 (function($){
-	$('p').aladeen();
-
+	var aladeenOptions = { // SET SOME OPTIONS
+		aladeen: true,
+		aladeen: 'slideUp'
+	};
+	$('p').aladeen(aladeenOptions).aladeen(); // PASS SOME OPTIONS
 })(jQuery);
